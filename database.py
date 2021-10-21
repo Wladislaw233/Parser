7e3209ffcbf0dd51f1f4b6037c4750a77f038948
wlalal <lunatob7@gmail.com>
2021-10-22 00:26:00 +0300

    add migration alembic

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 conn = sqlite3.connect("inter.db")
-sql="CREATE TABLE Processors(name TEXT, brand TEXT, price INTEGER, description TEXT, photo_link TEXT, product_link TEXT)"
+sql="CREATE TABLE Components(name TEXT, brand TEXT, price INTEGER, description TEXT, photo_link TEXT, product_link TEXT)"
 cursor = conn.cursor()
 cursor.execute(sql)
 conn.close()
